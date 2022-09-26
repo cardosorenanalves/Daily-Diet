39e39d5a4ba4a3239eb83fc20bd17e1d25f888db
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-26 14:06:57 -0300

    Feat: initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/pages/dashboard';
+
+export default function App() {
+  return (
+    <Dashboard />
+  );
+}
+
