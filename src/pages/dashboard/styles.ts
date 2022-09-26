42ad2b2fd48456afa4e2c52babe98294e83d7c29
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-26 15:00:48 -0300

    Feat: create styles of dashboard

---
+++ src/pages/dashboard/styles.ts
@@ -0,0 +1,61 @@
+import styled from "styled-components/native";
+
+export const Conteiner = styled.View`
+flex: 1;
+`;
+
+export const Header = styled.View`
+display: flex;
+flex-direction: row;
+
+width: 100%;
+height: 10%;
+
+justify-content: space-between;
+margin-top: 12%;
+padding: 0 5%;
+
+`;
+
+export const Logo = styled.Image`
+width: 82px;
+height: 37px;
+`;
+export const ProfilePhoto = styled.Image`
+width: 40px;
+height: 40px;
+border: 1px solid gray;
+
+border-radius: 50;
+`;
+
+export const Body = styled.View`
+display: flex;
+flex-direction: column;
+
+align-items: center;
+`;
+export const StatusCard = styled.View`
+display: flex;
+flex-direction: column;
+
+width: 86%;
+height: 40%;
+
+background-color: gray;
+align-items: center;
+justify-content: center;
+
+border-radius: 8;
+`;
+export const IconArrow = styled.View``;
+
+export const Percentage = styled.Text`
+
+`;
+export const InfoText = styled.Text`
+
+`;
+export const MealsDiv = styled.View``;
+export const MealsTitle = styled.View``;
+export const MealsButton = styled.View``;
