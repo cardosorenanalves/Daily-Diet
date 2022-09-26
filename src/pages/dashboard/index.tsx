d40cad4f8cdab7055b3fb64f6d8b76de68c5c7be
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-26 15:00:29 -0300

    Feat: dashboard init

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+    Conteiner,
+    Header,
+    Logo,
+    ProfilePhoto,
+    Body,
+    StatusCard,
+    IconArrow,
+    Percentage,
+    InfoText,
+    MealsDiv,
+    MealsTitle,
+    MealsButton
+
+} from './styles';
+
+import logoImg from '../../../assets/Logo.png'
 
 export function Dashboard(){
     return(
-        <View>
-            <Text>Dashboard</Text>
-        </View>
+        <Conteiner>
+            <Header>
+                <Logo source={logoImg}/>
+                <ProfilePhoto source={{
+                    uri: "https://github.com/cardosorenanalves.png"
+                }}/>
+            </Header>
+            <Body>
+                <StatusCard>
+                    <IconArrow/>
+                    <Percentage>98,86%</Percentage>
+                    <InfoText>das refeições dentro da dieta</InfoText>
+                </StatusCard>
+                <MealsDiv>
+                    <MealsTitle></MealsTitle>
+                    <MealsButton />
+                </MealsDiv>
+            </Body>
+        </Conteiner>
     )
 }
