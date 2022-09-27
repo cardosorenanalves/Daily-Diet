da7f3a326cd78e125f93183d230072ea1ab03fdd
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-27 10:24:13 -0300

    Feat: create new theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,28 @@
+export default{
+    colors: {
+        red_dark: '#C47F17',
+
+        red_mid: '#DBAC2C',
+        red_light: '#F1E9C9',
+
+        green_dark: '#4B2995',
+        green_mid: '#8047F8',
+
+        green_light: 'rgba(229, 240, 219, 1)',
+
+        gray_1: '#1B1D1E',
+
+        gray_2: '#333638',
+        gray_3: '#5C6265',
+        gray_4: '#B9BBBC',
+        gray_5: '#DDDEDF',
+        gray_6: '#FAFAFA',
+        white: '#FFFFFF'
+    },
+
+    fonts: {
+        
+        regular: 'Nunito_400Regular',
+        bold: 'Nunito_700Bold',
+    }
+}
