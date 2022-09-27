92759f29f7eb3076e7c13b0565f1c6dd7b0b8773
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-27 10:25:02 -0300

    Feat: styles of button and title

--- src/pages/dashboard/styles.ts
+++ src/pages/dashboard/styles.ts
@@ -13,7 +13,7 @@ height: 10%;
 
 justify-content: space-between;
 margin-top: 12%;
-padding: 0 5%;
+padding: 0 6%;
 
 `;
 
@@ -39,23 +39,49 @@ export const StatusCard = styled.View`
 display: flex;
 flex-direction: column;
 
-width: 86%;
-height: 40%;
+width: 327px;
+height: 102px;
 
-background-color: gray;
+background-color: ${({theme}) => theme.colors.green_light};
 align-items: center;
 justify-content: center;
 
 border-radius: 8;
+margin-bottom: 40px;
 `;
 export const IconArrow = styled.View``;
 
 export const Percentage = styled.Text`
+font-size: 32px;
+font-weight: ${({theme}) => theme.fonts.bold};
 
 `;
 export const InfoText = styled.Text`
 
 `;
-export const MealsDiv = styled.View``;
-export const MealsTitle = styled.View``;
-export const MealsButton = styled.View``;
+export const MealsDiv = styled.View`
+
+
+`;
+export const MealsTitle = styled.Text`
+font-size: 16px;
+
+margin-bottom: 8px;
+`;
+export const MealsButton = styled.TouchableOpacity`
+display: flex;
+width: 327px;
+height: 50px;
+background-color: ${({theme}) => theme.colors.gray_1};
+
+border-radius: 6;
+align-items: center;
+justify-content: center;
+
+`;
+
+export const TitleButton = styled.Text`
+font-size: 16px;
+color: white;
+
+`;
