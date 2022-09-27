7466563627db709d66980399dc75ff9c75ebf19e
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-27 12:35:08 -0300

    Feat: new styles statistics

---
+++ src/pages/statistics/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components/native"
+
+export const Conteiner = styled.View`
+flex: 1;
+`;
+
+export const Header = styled.View`
+display: flex;
+width: 100%;
+height: 200px;
+
+background-color: ${({theme}) => theme.colors.green_light};
+
+align-items: center;
+justify-content: center;
+`;
+
+export const Percentage = styled.Text``;
+
+export const Body = styled.View``;
+
+export const Title = styled.Text``;
+
+export const CardLarge = styled.View``;
+
+export const Number = styled.Text``;
+
+export const Info = styled.Text``;
+
+export const SmallDiv = styled.View``;
+
+export const CardSmall = styled.View``;
