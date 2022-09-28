13ee4fc5adc8c34fa9e9a85facf8983f9087c5a7
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-28 14:16:34 -0300

    Feat: styles of successStats

---
+++ src/pages/successStats/styles.tsx
@@ -0,0 +1,56 @@
+import styled from "styled-components/native"
+
+
+export const Conteiner = styled.View`
+flex: 1;
+display: flex;
+
+align-items: center;
+justify-content: center;
+`;
+
+export const Title = styled.Text`
+font-weight: 700;
+font-size: 24px;
+
+color: #639339;
+`;
+
+export const Subtitle = styled.Text`
+font-weight: 400;
+font-size: 16px;
+color: #1B1D1E;
+`;
+
+export const ImageIlustration = styled.Image`
+margin-top: 40px;
+margin-bottom: 48px;
+
+`;
+
+export const ButtonReturn = styled.TouchableOpacity`
+display: flex;
+
+align-items: center;
+justify-content: center;
+
+width: 191px;
+height: 50px;
+
+background: #333638;
+border-radius: 6px;
+`;
+
+export const Strong = styled.Text`
+font-weight: 700;
+font-size: 16px;
+color: #1B1D1E;
+`;
+
+export const ButtonContent = styled.Text`
+font-weight: 700;
+font-size: 14px;
+
+color: #FFFFFF;
+
+`;
