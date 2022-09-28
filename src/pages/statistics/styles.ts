df912a38b58f32e09991a3f34719e921c6a65a13
Renan Cardoso <renancardoso0307@gmail.com>
2022-09-28 10:23:27 -0300

    Feat: styles complete

--- src/pages/statistics/styles.ts
+++ src/pages/statistics/styles.ts
@@ -1,7 +1,9 @@
+import { ArrowLeft } from "phosphor-react-native";
 import styled from "styled-components/native"
 
 export const Conteiner = styled.View`
 flex: 1;
+background-color: ${({theme}) => theme.colors.green_light};
 `;
 
 export const Header = styled.View`
@@ -15,18 +17,81 @@ align-items: center;
 justify-content: center;
 `;
 
-export const Percentage = styled.Text``;
+export const Percentage = styled.Text`
+font-weight: 700;
+font-size: 32px;
+`;
 
-export const Body = styled.View``;
+export const Body = styled.View`
+display: flex;
+height: 100%;
+background-color: ${({theme}) => theme.colors.white};
+border-radius: 20px;
 
-export const Title = styled.Text``;
+align-items: center;
+`;
 
-export const CardLarge = styled.View``;
+export const Title = styled.Text`
+font-weight: 700;
+font-size: 16px;
 
-export const Number = styled.Text``;
+margin-top: 33px;
+margin-bottom: 23px;
+`;
 
-export const Info = styled.Text``;
+export const CardLarge = styled.View`
+display: flex;
+width: 327px;
+height: 89px;
 
-export const SmallDiv = styled.View``;
+align-items: center;
+justify-content: center;
 
-export const CardSmall = styled.View``;
+background-color:  #EFF0F0;
+border-radius: 8px;
+
+margin-bottom: 12px;
+`;
+
+export const Number = styled.Text`
+font-weight: 700;
+font-size: 24px;
+`;
+
+export const Info = styled.Text`
+font-weight: 400;
+font-size: 14px;
+`;
+
+export const SmallDiv = styled.View`
+width: 327px;
+display: flex;
+flex-direction: row;
+
+justify-content: space-between;
+`;
+
+export const CardSmall = styled.View`
+display: flex;
+flex-direction: column;
+
+width: 157.5px;
+height: 107px;
+
+background-color: #E5F0DB;
+
+align-items: center;
+justify-content: center;
+
+
+border-radius: 8px;
+`;
+
+export const ArrowLeftIcon = styled(ArrowLeft)`
+font-size: 32px;
+color: #639339;
+`
+
+export const BackButton = styled.TouchableOpacity`
+margin-right: 80%;
+`;
